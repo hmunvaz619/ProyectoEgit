c0e4ad2b0a7ee96d5089fcb617399f7a3e6b56dc
HugoMuniz <hmunvaz619@g.educaand.es>
2022-12-13 11:55:10 +0100

    comit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int numero1, numero2, resultado;
+		System.out.print("Introduce primer número: ");
+		numero1 = sc.nextInt();
+		System.out.print("Introduce segundo número: ");
+		numero2 = sc.nextInt();
+	}
+
+}
